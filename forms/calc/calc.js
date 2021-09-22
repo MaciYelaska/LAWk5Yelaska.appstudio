5a11fb8c7e7f26571fea7ec9f686a20c42400020
MaciYelaska <maciyelaska@creighton.edu>
2021-09-22 15:04:37 -0500

    strings branch

---
+++ forms/calc/calc.js
@@ -0,0 +1,11 @@
+/* function calculate (n1, n2) {
+  let answer = n1 + n2
+  return answer
+}
+  
+let num1 = Number(prompt("Enter a whole number:"))
+let num2 = Number(prompt("Enter a second whole number: "))
+
+let summedNumbers = calculate(num1,num2)
+  console.log(`The sum of ${num2} and ${num1} is ${summedNumbers}.`)
+*/
